0b580563cc7c460374f580aa6e601b1bf2a4a674
Dustin J. Mitchell <dustin@mozilla.com>
2020-08-07 17:45:10 -0400

    move analysis to its own file

---
+++ src/ui/analysis.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import DataStoreContext from './datastore';
+
+/**
+ * A singleton class for calculating analysis of a run.
+ *
+ * This is full of `get` methods, which use React hooks to ensure that long calculations are
+ * re-done only when necessary.  All of these functions should only be called within a
+ * render function.
+ *
+ * When writing new functions, follow the pattern of destructuring `this` to get any
+ * other analyses, then performing additional analysis inside of `useMemo`, including
+ * all of the consumed values in the second argument to `useMemo`.
+ */
+class Analysis {
+  /**
+   * Get the DataStore object itself
+   */
+  get datastore() {
+    return React.useContext(DataStoreContext);
+  }
+
+  /**
+   * Get the completed analysis from the DataStore object, with the given
+   * options.
+   */
+  analyze(options) {
+    const {datastore} = this;
+    return React.useMemo(
+      () => datastore.analyze(options),
+      [datastore, options]);
+  }
+
+  /**
+   * Get the final state from datastore.analyze
+   */
+  get finalState() {
+    const {state} = this.analyze({interval: Infinity, metrics: {}});
+    return state;
+  }
+
+  get overProvisionedWorkers() {
+    const {finalState} = this;
+    return React.useMemo(
+      () => {
+        let count = 0;
+        
+        // we only need to look at workers that have shut down, as running
+        // workers may still yet take a task; and only look at resolved tasks
+        // as a task cannot be running on a shut-down worker.
+        for (let {resolvedTasks} of finalState.shutdownWorkers.values()) {
+          console.log(resolvedTasks.size);
+          if (resolvedTasks.size === 0) {
+            count++;
+          }
+        }
+
+        return count;
+      },
+      [finalState]);
+  }
+
+  /**
+   * Collect all of the summary statistics, in the form {title, description, value}
+   */
+  get summaryStatistics() {
+    const {datastore, overProvisionedWorkers} = this;
+    const statistics = [];
+
+    statistics.push({
+      title: 'Number of Events',
+      description: 'Total number of simulation events during the simulation phase, including contextual events.',
+      value: datastore.events.length,
+    });
+
+    statistics.push({
+      title: 'Simulation Duration',
+      description: 'Total duration of the simulation phase.',
+      value: `${datastore.duration}ms`,
+    });
+
+    statistics.push({
+      title: 'Overprovisioned Workers',
+      description: 'Number of workers that started up, did no work, and shut down again.',
+      value: `${overProvisionedWorkers} workers`,
+    });
+
+    return statistics;
+  }
+}
+
+export default new Analysis();
--- src/ui/views/Summary.jsx
+++ src/ui/views/Summary.jsx
@@ -1,54 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { hot } from 'react-hot-loader';
-import DataStoreContext from '../datastore';
+import analysis from '../analysis';
 
 const statistics = [];
 
-statistics.push({
-  title: 'Number of Events',
-  description: 'Total number of simulation events during the simulation phase, including contextual events.',
-  calculate(datastore) {
-    return datastore.events.length;
-  }
-});
-
-statistics.push({
-  title: 'Simulation Duration',
-  description: 'Total duration of the simulation phase.',
-  calculate(datastore) {
-    return `${datastore.duration}ms`;
-  }
-});
-
-statistics.push({
-  title: 'Overprovisioned Workers',
-  description: 'Number of workers that started up, did no work, and shut down again.',
-  calculate(datastore) {
-    const overProvisioned = new Set();
-    for (let [time, evt, ...rest] of datastore.events) {
-      switch (evt) {
-        case 'worker-started': {
-          overProvisioned.add(rest[0]);
-          break;
-        }
-        case 'task-started': {
-          overProvisioned.delete(rest[1]);
-          break;
-        }
-      }
-    }
-
-    return `${overProvisioned.size} workers`;
-  }
-});
 
 const Summary = () => {
-  const ds = React.useContext(DataStoreContext);
   return (
     <>{
-      statistics.map(({title, calculate}) => {
-        const value = React.useMemo(() => calculate(ds), [ds]);
+      analysis.summaryStatistics.map(({title, value}) => {
         return (
           <div key={title}>
             <em>{title}:</em>{' '}{value}
